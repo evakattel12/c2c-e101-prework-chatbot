9d9ac50a81ae9df466f57db1db3d9c27f4be00a9
evakattel12 <eva.kattel.12@gmail.com>
2025-09-29 20:12:03 -0500

    Update main.py
    main.py shows the changes copied from codespace file.

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Store User Chatbot!")
+name = input("Please enter your name: ")
+age = int(input("Please enter your age: "))
+
+print(f"Hello {name}, you are {age} years old.")
