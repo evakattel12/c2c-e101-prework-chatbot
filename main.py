76833548a880165a93b8d60ca317e8adb45c581a
evakattel12 <eva.kattel.12@gmail.com>
2025-11-10 12:05:15 +0000

    Added in banking information function. - EK 11/10/25

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ name = ""
 age = ""
 ###     FUNCTION DEFINITIONS     ###
 
-def user_introduction(): # This function lets the user input their name and age, which is then returned to the global variables for further use.
+def user_introduction(name, age): # This function lets the user input their name and age, which is then returned to the global variables for further use.
     print("Hello, I am Dora, welcome to the Financia Bank Chatbot! You may sign up to access advanced chatbot options.")
     name = input("Please enter your name: ")
     age = int(input("Please enter your age: "))
@@ -16,7 +16,6 @@ def bot_prompt(): # This function prompts the user with 4 base choices. It only 
 
 username_signup = ""
 user_signup_password = ""
-
 def user_prompt(user_response): # This function displays different outputs for each of the four inputs. The first one is for signing up, the second is for logging in once you signed up, the third is to learn how to make a basic account with or without signing in, and the fourth is the exit.
     bot_run = True
     while bot_run:
@@ -46,26 +45,26 @@ def login_system(username_signup, user_signup_password): # This function lets th
     if username_signup == "" and user_signup_password == "":
         print("You currently don't have an account. Please sign up to create one.")
         signup_system()
-    else:
-        username_login = username_signup
-        userpass_login = user_signup_password
-        print("Please log in.")
+    username_login = username_signup
+    userpass_login = user_signup_password
+    print("Please log in.")
+    username_correct = input("Username: ")
+    userpass_correct = input("Password: ")
+    if username_correct == username_login and userpass_correct == userpass_login:
+            print(f"Welcome {username_correct} to your banking and financials account.\n Here are some advanced features you can use to navigate your account.")
+            adv_features = int(input("1. Bank Account Information\n 2. Financial Planning\n 3. Cards, Accounts, & Rates\n 4. Exit"))
+            advanced_features(adv_features)
+    elif username_correct != username_login and userpass_correct != userpass_login:
+        print("Please try again.")
         username_correct = input("Username: ")
         userpass_correct = input("Password: ")
-        while str(username_correct) != str(username_login) and str(userpass_correct) != str(userpass_login):
-            print("Please try again.")
-            username_correct = input("Username: ")
-            userpass_correct = input("Password: ")
-            if str(username_correct) == str(username_login) and str(userpass_correct) == str(userpass_login):
-                print(f"Welcome {username_correct} to your banking and financials account.\n Here are some advanced features you can use to navigate your account.")
-                adv_features = int(input("1. Bank Account Information\n 2. Financial Planning\n 3. Cards, Accounts, & Rates\n 4. Exit"))
-                advanced_features(adv_features)
+        
 
 def advanced_features(feature_num): # This function uses the advanced features of the account and bot to show 4 more options. The first 
     bot_run = True
     while bot_run:
         if (feature_num==1):
-            print("Thanks for creating an account! Please log into your new account to access advanced options.")
+            banking_info(age, account_amount)
             feature_num = int(input("1. Bank Account Information\n 2. Financial Planning\n 3. Cards, Accounts, & Rates\n 4. Exit"))
         elif (feature_num==2):
             print("Here is a comparison of your account using both compound ")
@@ -78,11 +77,22 @@ def advanced_features(feature_num): # This function uses the advanced features o
         else:
             feature_num = input("Invalid input. 1. Bank Account Information\n 2. Financial Planning\n 3. Cards, Accounts, & Rates\n 4. Exit")
 
+account_amount = 0
+def banking_info(name, account_amount):
+    print(f"Hello {name}, here is your banking information.")
+    if account_amount == 0:
+        account_amount = int(input("Please put in some money into your account: "))
+        return account_amount
+    else: 
+        print(f"Current account amount: ${account_amount}")
+    
 def account_creation_help():
     print("It seems that you are trying to make an account, let's go through the steps\n 1. Go through the chatbot's basic entry. Select 'Sign Up' once you reach it.\n 2. Go through the sign up process.\n 3. All done! Now go back and log into your new account!")
 
 def main():
-    user_introduction()
-    bot_prompt()
+    user_introduction(name, age)
+    banking_info(age, account_amount)
+    #bot_prompt()
+    advanced_features(1)
 
 main()
